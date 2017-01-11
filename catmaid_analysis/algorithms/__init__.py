26c03e8e9ab84b9df5fa4fe50ffee8ec2d6522f7
Russel Torres <russtorres@gmail.com>
2017-01-11 11:42:19 -0500

    init: removing useless line

--- catmaid_analysis/algorithms/__init__.py
+++ catmaid_analysis/algorithms/__init__.py
@@ -5,6 +5,5 @@ from . import morphology
 from . import skeleton
 from . import utils
 from . import smoothing
-#from . import skeleton_json_new_to_old
 
 __all__ = ['graph', 'morphology', 'skeleton', 'utils', 'smoothing']
